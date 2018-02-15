e4223dc1e5cdd01ab804f3a994cd3b7437ed0e15
Susanna Brown <sbrown@tinkerbird.stats.ox.ac.uk>
2018-02-15 16:48:34 +0000

    added htilde

--- GaussDLM.R
+++ GaussDLM.R
@@ -1,8 +1,8 @@
 # generate some observations of Y:
-GaussDLM <- function(nobs, a, b, sigma.x, sigma.y){
+GaussDLM <- function(nobs, a, b, sigma.x, sigma.y, x0=0){
   X <- numeric(nobs)
   Y <- numeric(nobs)
-  X[1] <- rnorm(1,0, sigma.x)
+  X[1] <- rnorm(1,x0, sigma.x)
   for (t in 1:nobs){
     Y[t] <- rnorm(1,b*X[t], sigma.y)
     X[t+1] <- rnorm(1,a*X[t], sigma.x)
@@ -13,4 +13,9 @@ GaussDLM <- function(nobs, a, b, sigma.x, sigma.y){
 # forward transition density for X:
 q <- function(x1,x2){
   dnorm(a*x1, x2, sigma.x) #first and second arguments swapped to less obvious order for vectorisation
+}
+
+# increment for "h1" in paper
+htilde <- function(x, x_new){
+  x_new
 }
