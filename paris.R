a194caffea1c1c885fd1c84f6cbb738c15064028
Susanna Brown <sbrown@tinkerbird.stats.ox.ac.uk>
2018-02-15 12:38:18 +0000

    more complete algorithm

--- paris.R
+++ paris.R
@@ -1,12 +1,19 @@
-paris <- function(ksi, omega, tau, Ntilde){
-  # skeleton code for PaRIS algorithm
-  # Requires functions: PF, q, htilde
-  N <- length(ksi)
-  c(ksi_new, omega_new) <- PF(ksi, omega) #invoke the vanilla SMC update
+# skeleton code for increment of PaRIS algorithm
+# Requires functions: PF, q, htilde. q and htilde must be compatible with vectorised first argument.
+
+paris.inc <- function(ksi, omega, tau, Ntilde){
+  N <- length(ksi) #number of particles (could be passed in as argument)
+  list(ksi_new, omega_new) <- PF(ksi, omega) #invoke the vanilla SMC update
   for (i in 1:N){
-    for (j in 1:Ntilde){
-      J <- sample(1:N, Ntilde, replace=F, prob = omega*q(ksi, ksi_new))
-    }
-    tau_new <- Ntilde^(-1)*(sum(tau[J])+ sum(htilde(ksi[J], ksi_new)))
+    J <- sample(1:N, Ntilde, replace=F, prob = omega*q(ksi, ksi_new[i])) #choose indices
+    tau_new[i] <- (sum(tau[J]) + sum(htilde(ksi[J], ksi_new[i]))) / Ntilde #update particle estimate of h
   }
+  list(ksi=ksi_new, omega=omega_new, tau=tau_new)
+}
+
+
+
+# wrapper for all iterations if using offline
+paris <- function(){
+  
 }
