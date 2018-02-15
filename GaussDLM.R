f3cbd16dc8ee6baae6310c20e57b43800f08ea67
Susanna Brown <sbrown@tinkerbird.stats.ox.ac.uk>
2018-02-15 12:37:57 +0000

    added forward density function

--- GaussDLM.R
+++ GaussDLM.R
@@ -1,3 +1,4 @@
+# generate some observations of Y:
 GaussDLM <- function(nobs, a, b, sigma.x, sigma.y){
   X <- numeric(nobs)
   Y <- numeric(nobs)
@@ -7,4 +8,9 @@ GaussDLM <- function(nobs, a, b, sigma.x, sigma.y){
     X[t+1] <- rnorm(1,a*X[t], sigma.x)
   }
   Y
+}
+
+# forward transition density for X:
+q <- function(x1,x2){
+  dnorm(a*x1, x2, sigma.x) #first and second arguments swapped to less obvious order for vectorisation
 }
