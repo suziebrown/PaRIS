a953532c5251f2e4b66f38998221643bca352465
marcopalma3 <mapalm3@hotmail.it>
2018-02-16 11:15:08 +0000

    Added iteration

--- forwardFFBSm.R
+++ forwardFFBSm.R
@@ -1,22 +1,23 @@
 ###Forward-only implementation of Forward-filtering backward-smoothing 
 
-forwardFFBSm<-function(ksi,omega,tau){
+forwardFFBSm.iter<-function(ksi,omega,tau){
   
   check.dim<-c(length(ksi),length(omega),length(tau))
   if(all(check.dim==check.dim[1])==FALSE) stop("The length of ksi, omega and tau must be the same!")
   
   N<-length(ksi)
-  list(ksi_new, omega_new)<-PF(ksi,omega)
+  list(ksi_new, omega_new)<-PF(N, ksi, omega, y, param)
   
   transition<-numeric(N)
   tau_new<-numeric(N)
   
   for (i in 1:N){
-    transition<-omega*q.func(ksi,ksi_new[i])
-    tau_new[i]<-sum(transition[i]/sum(transition[i])*(tau+h.func(ksi,ksi_new[i])))
+    transition<-omega*d.transition(ksi,ksi_new[i])
+    tau_new[i]<-sum(transition/sum(transition)*(tau+htilde(ksi,ksi_new[i])))
   }
   
-  smoothing.distrib<-sum(omega/sum(omega)*tau_new)
-  return("ParticleSample"=new.particle.sample,"Auxiliary"=tau_new,"SmoothingDistribution"=smoothing.distrib)
+  #smoothing.distrib<-sum(omega/sum(omega)*tau_new)
+  #return("ParticleSample"=new.particle.sample,"Auxiliary"=tau_new,"SmoothingDistribution"=smoothing.distrib)
+  return(list(ksi=ksi_new, omega=omega_new, tau=tau_new))
 }
 
