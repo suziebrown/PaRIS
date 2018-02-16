9949c0053b167dfbea0c65cc04edaf280f606bdf
Francesca Panero <panero@thornbill.stats.ox.ac.uk>
2018-02-16 12:13:59 +0000

    added comments for inputs in functions

--- PF.R
+++ PF.R
@@ -1,20 +1,25 @@
-r.transition <- function(N, mu, sigma){ # choose any f transition dist
+r.transition <- function(N, mu, sigma){ # choose any transition distribution for x_t|x_{t-1}
+  # N number of particles
+  # other inputs are parameters of the distribution
   rnorm(N, mu, sigma)
 }
-#
-d.emission <- function(y, mu, sigma){ # choose g(y|x) emission dist
+
+d.emission <- function(yt, mu, sigma){ # choose emission distribution for y_t|x_t
+  # yt=y_t: observation at time t
+  # other inputs are parameters of the distribution
   dnorm(y, mu, sigma)
 }
 
-r.initial_distr <- function(N, mu, sigma){ # choose any mu initial dist
+r.initial_distr <- function(N, mu, sigma){ # choose any initial distribution for x_1
+  # N number of particles
+  # other inputs are parameters of the distribution
   rnorm(N, mu, sigma)
 }
 
 PF.smooth <- function(N, y, param){
   # N: number of particles
   # y=(y_1,...,y_T): observations
-  # param: parameters for initial/trasition/emission distributions
-  # in my case c(mean.x, sigma.x, sigma.y)
+  # param: parameters for initial/trasition/emission distributions (in "super Gaussian" case c(mean.x, sigma.x, sigma.y))
   Nobs <- length(y)
   xtilde <- r.initial_distr(N, param[1], param[2]) 
   W1 <- d.emission(y[1], xtilde, param[3]) 
@@ -39,19 +44,14 @@ PF.smooth <- function(N, y, param){
 out <- PF.smooth(10, c(1,3,2,3,3,7,10,5,6,11), c(1,10,4))
 out
 
-
-
-
 PF.filter <- function(N, y, param){
   # N: number of particles
-  # y=(y_1,...,y_T): observations
-  # param: parameters for initial/trasition/emission distributions
-  # in my case c(mean.x, sigma.x, sigma.y)
+  # y=(y_1,...,y_Nobs): observations
+  # param: parameters for initial/trasition/emission distributions (in "super Gaussian" case c(mean.x, sigma.x, sigma.y))
   Nobs <- length(y)
   xtilde <- r.initial_distr(N, param[1], param[2])
   W1 <- d.emission(y[1], xtilde, param[3])
   W1 <- W1/sum(W1)
-  #xtilde <- sample(xtilde, N, prob=W1, replace=TRUE)
   Xtilde <- matrix(0, Nobs, N)
   Xtilde[1,] <- xtilde
   W <- matrix(0, Nobs, N)
@@ -67,11 +67,15 @@ PF.filter <- function(N, y, param){
 out1 <- PF.filter(10, c(1,3,2,3,3,7,10,5,6,11), c(1,10,4))
 out1
 
-
-PF.iter <- function(N, ksi, omega, y, param){
+PF.iter <- function(N, ksi, omega, yt, param){
+  # N: number of particles
+  # ksi=(ksi_t^1,...,ksi_t^N): states at time t of each particle
+  # omega=(omega_t^1,...,omega_t^N): weights at time t for each particle
+  # yt=y_t: observation at time t
+  # param: parameters for initial/trasition/emission distributions (in "super Gaussian" case c(mean.x, sigma.x, sigma.y))
   index <- sample(1:N, N, replace=TRUE, prob=omega)
   Xtilde <- r.transition(N, ksi, param[2])
-  W <- d.emission(y, Xtilde, param[3])
+  W <- d.emission(yt, Xtilde, param[3])
   W <- W/sum(W)
   return(list(ksi=Xtilde, omega=W))
 }
