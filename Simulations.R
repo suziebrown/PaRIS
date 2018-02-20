d91006544d3326ba59c4bb6cc87a38b197a75dad
Francesca Panero <panero@thornbill.stats.ox.ac.uk>
2018-02-20 17:13:37 +0000

    simulation tuesday

--- Simulations.R
+++ Simulations.R
@@ -11,7 +11,7 @@ par.trans<-c(0.7,0.2)
 par.em<-c(1,1)
 
 ## generate a y
-Nobs=5000
+Nobs=5001
 y<-obs.sim(Nobs, par.init, par.trans, par.em)
 
 
@@ -59,6 +59,20 @@ for(j in 1:50){
   }
 }
 
+system.time(for(j in 1:50){
+  for (k in 1:5){
+    tau.FFBSm<-forwardFFBSm(y[1:(1000*k)], N.FFBSm, par.init, par.trans, par.em)$tau
+    h2.FFSBm[j,k]<-mean(tau.FFBSm[dim(tau.FFBSm)[1],])
+  }
+})
+
+system.time(for(j in 1:50){
+  for (k in 1:5){
+    tau.paris<-paris(y[1:(1000*k)], N.paris, Ntilde, par.init, par.trans, par.em)$tau
+    h2.paris[j,k]<-mean(tau.paris[dim(tau.paris)[1],])
+  }
+})
+
 ##~~~
 ## increment for "h3" in paper (sort of)
 htilde <- function(x, x_new){
@@ -80,21 +94,61 @@ for(j in 1:50){
   }
 }
 
+system.time(for(j in 1:50){
+  for (k in 1:5){
+    tau.paris<-paris(y[1:(1000*k)], N.paris, Ntilde, par.init, par.trans, par.em)$tau
+    h3.paris[j,k]<-mean(tau.paris[dim(tau.paris)[1],])
+  }
+})
+
+system.time(for(j in 1:50){
+  for (k in 1:5){
+    tau.FFBSm<-forwardFFBSm(y[1:(1000*k)], N.FFBSm, par.init, par.trans, par.em)$tau
+    h3.FFSBm[j,k]<-mean(tau.FFBSm[dim(tau.FFBSm)[1],])
+  }
+})
+
+################### Frappi's h3 with 5001 obs
+h3.FFSBm.5001<-matrix(0,nrow=50,ncol=5)
+colnames(h3.FFSBm.5001)<-c("1000","2000","3000","4000","5000")
+h3.paris.5001<-matrix(0,nrow=50,ncol=5)
+colnames(h3.paris.5001)<-c("1000","2000","3000","4000","5000")
+for(j in 1:50){
+  for (k in 1:5){
+    tau.FFBSm.5001<-forwardFFBSm(y[1:((1000*k)+1)], N.FFBSm, par.init, par.trans, par.em)$tau
+    h3.FFSBm.5001[j,k]<-mean(tau.FFBSm.5001[dim(tau.FFBSm.5001)[1],])
+    tau.paris.5001<-paris(y[1:((1000*k)+1)], N.paris, Ntilde, par.init, par.trans, par.em)$tau
+    h3.paris.5001[j,k]<-mean(tau.paris.5001[dim(tau.paris.5001)[1],])
+  }
+}
+model_y.5001<- SSModel(y ~ -1 +SSMcustom(Z = Zt, T = Tt, R = Rt, Q = Qt, a1 = a1, P1 = P1, P1inf = P1inf),H = Ht)
+distsmooth_y.5001 <- KFS(model_y.5001,smoothing="disturbance")  
+alpha.h3.5001<-distsmooth_y.5001$att*c(0,distsmooth_y.5001$att[-length(distsmooth_y.5001$att)])
+set.ylim3<-range(c(t(h3.FFSBm.5001)/((1:5)*1000+1)),c(t(h3.paris.5001)/((1:5)*1000+1)),cumsum(alpha.h3.5001)[(1:5)*1000]/((1:5)*1000+1))*c(0.9,1.1)
+boxplot(t(t(h3.FFSBm.5001)/((1:5)*1000+1)),xlab="t",ylab=expression(paste("estimate of h3"[t],"/t")),cex.lab=1.2, ylim=set.ylim3)
+points(cumsum(alpha.h3.5001)[(1:5)*1000]/((1:5)*1000+1),pch=8,col=2)
+boxplot(t(t(h3.paris)/((1:5)*1000+1)),xlab="t",ylab=expression(paste("estimate of h3"[t],"/t")),cex.lab=1.2, ylim=set.ylim3)
+points(cumsum(alpha.h3)[(1:5)*1000-1]/((1:5)*1000),pch=8,col=2)
+
 
 ###Producing the disturbance smoother (exact value of the smoothed sufficient statistic)
 ###refer to https://cran.r-project.org/web/packages/KFAS/vignettes/KFAS.pdf
 library(KFAS)
 Zt <- par.em[1]
-Ht <- matrix(NA)
+#Ht <- matrix(NA)
+Ht <- 1 
 Tt <- par.trans[1]
 Rt <- par.trans[2]
-Qt <- matrix(NA)
+#Qt <- matrix(NA)
+Qt <- 1
 a1 <- 0
 P1 <- 1
 P1inf <- 0
 model_y<- SSModel(y ~ -1 +SSMcustom(Z = Zt, T = Tt, R = Rt, Q = Qt, a1 = a1, P1 = P1, P1inf = P1inf),H = Ht)
-model_y <- fitSSM(model_y, c(log(var(y)), log(var(y))),method = "BFGS")$model
-distsmooth_y <- KFS(model_y)  
+#model_y <- fitSSM(model_y, c(log(var(y)), log(var(y))),
+#                  method = "BFGS")$model
+distsmooth_y <- KFS(model_y)#,smoothing="disturbance")  
+points(cumsum(distsmooth_y$att)[(1:5)*1000]/((1:5)*1000),pch=8,col=3)
 
 ###Produce the plot
 
@@ -103,18 +157,18 @@ par(mfrow=c(3,2))
 
 set.ylim1<-range(c(t(h1.FFSBm)/((1:5)*1000)),c(t(h1.paris)/((1:5)*1000)))*c(1.1,1.1)
 boxplot(t(t(h1.FFSBm)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h1"[t],"/t")),cex.lab=1.2, main="Forward-only FFBSm",ylim=set.ylim1)
-points(cumsum(distsmooth_y$alphahat)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
-boxplot(t(t(h1.paris)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h1"[t],"/t")),cex.lab=1.2, main="PaRIS",ylim=set.ylim1)
-points(cumsum(distsmooth_y$alphahat)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
+points(cumsum(distsmooth_y$att)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
+boxplot(t(t(h1.paris*2)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h1"[t],"/t")),cex.lab=1.2, main="PaRIS",ylim=set.ylim1)
+points(cumsum(distsmooth_y$att)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
 
-set.ylim2<-range(c(t(h2.FFSBm)/((1:5)*1000)),c(t(h2.paris)/((1:5)*1000)))*c(1.1,1.1)
+set.ylim2<-range(c(t(h2.FFSBm)/((1:5)*1000)),c(t(h2.paris*2)/((1:5)*1000)),cumsum(distsmooth_y$att^2)[(1:5)*1000]/((1:5)*1000))*c(0.9,1.1)
 boxplot(t(t(h2.FFSBm)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h2"[t],"/t")),cex.lab=1.2, ylim=set.ylim2)
-points(cumsum(distsmooth_y$alphahat^2)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
-boxplot(t(t(h2.paris)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h2"[t],"/t")),cex.lab=1.2, ylim=set.ylim2)
-points(cumsum(distsmooth_y$alphahat^2)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
+points(cumsum(distsmooth_y$att^2)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
+boxplot(t(t(h2.paris*2)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h2"[t],"/t")),cex.lab=1.2, ylim=set.ylim2)
+points(cumsum(distsmooth_y$att^2)[(1:5)*1000]/((1:5)*1000),pch=8,col=2)
 
-set.ylim3<-range(c(t(h3.FFSBm)/((1:5)*1000)),c(t(h3.paris)/((1:5)*1000)))*c(0.9,1.1)
 alpha.h3<-distsmooth_y$alphahat*c(0,distsmooth_y$alphahat[-length(distsmooth_y$alphahat)])
+set.ylim3<-range(c(t(h3.FFSBm)/((1:5)*1000)),c(t(h3.paris)/((1:5)*1000)),cumsum(alpha.h3)[(1:5)*1000-1]/((1:5)*1000))*c(0.9,1.1)
 boxplot(t(t(h3.FFSBm)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h3"[t],"/t")),cex.lab=1.2, ylim=set.ylim3)
 points(cumsum(alpha.h3)[(1:5)*1000-1]/((1:5)*1000),pch=8,col=2)
 boxplot(t(t(h3.paris)/((1:5)*1000)),xlab="t",ylab=expression(paste("estimate of h3"[t],"/t")),cex.lab=1.2, ylim=set.ylim3)
